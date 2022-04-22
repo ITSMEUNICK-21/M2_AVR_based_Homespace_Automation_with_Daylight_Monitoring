8a0e7758875f5b26a8030ab7824c948895e17c53
HEGDE NIKHIL SURESH <nikhilsureshhegde21@gmail.com>
2022-04-22 22:17:30 +0530

    Update all_functions.h

--- 3_Implementation/inc/all_functions.h
+++ 3_Implementation/inc/all_functions.h
@@ -15,11 +15,32 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+/**
+* @brief Performs initializations associated with ADC.
+*/
 void InitADC(void);
+
+/**
+* @brief Performs initializations associated with peripherals of Atmega328p microcontroller.
+*/
 void peripheral_init(void);
+
+/**
+* @brief Performs initializations associated with timer registers of Atmega328p microcontroller.
+*/
 void timerreg_init(void);
 
+/**
+* @brief Reads the analog signal from the selected channel and returns the digital equivalent value.
+* @param[in] ch
+* @return Digital equivalent of the analog signal
+*/
 uint16_t ReadADC(uint8_t ch);
 
+/**
+* @brief Detects the presence of an object and calculates the distance.
+* Formula : (For 16 MHz clock), Distance = ((double)count/233.25) 
+* @return Distance value in centimeters(cm).
+*/
 unsigned int read_ultrasonic();
 
